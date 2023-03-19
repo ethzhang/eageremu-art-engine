aa361dd6256c542237df66da910ff50e9d4b654f
ethzhang <ethanzhang.cn@gmail.com>
2023-03-19 22:27:04 +0800

    feat: Add program to generate selected file ID array and export it as JavaScript module.

--- readFilesName.js
+++ readFilesName.js
@@ -1,11 +1,37 @@
 const fs = require('fs');
+const path = require('path');
 
-const selectedFiles = './selected/';
-const selectedIds = [];
+const folderPath = './selected/';
+const outputFilePath = './selectedIds.js';
 
-fs.readdir(selectedFiles, (err, files) => {
-  files.forEach((file) => {
-    selectedIds.push(file.split('.')[0]);
+// 读取目录下的所有文件
+fs.readdir(folderPath, function (err, files) {
+  if (err) {
+    console.error("读取目录失败：", err);
+    return;
+  }
+
+  const selectedIds = [];
+
+  // 遍历每个文件名，并提取数字后存入 selectedIds 数组
+  files.forEach(function (file) {
+    const regExpResult = /\d+/.exec(file);
+    if (regExpResult === null) {
+      console.warn("文件名格式错误：", file);
+    } else {
+      const id = regExpResult[0];
+      selectedIds.push(`'${id}'`);
+    }
   });
-  console.log('selectedIds', selectedIds);
+
+  // 生成导出文件名数组的 JS 文件
+  const fileContent = "const selectedIds = [" + selectedIds.join(",") + "];\nmodule.exports = selectedIds;";
+  fs.writeFile(outputFilePath, fileContent, function (err) {
+    if (err) {
+      console.error("写入文件失败：", err);
+    } else {
+      console.log("选中文件的 ID 已保存到文件：", outputFilePath);
+    }
+  });
+
 });
